8374834380255c576a9e72a61430ce4a66eff43b
N700S <shiromaruedit@MacBook-Pro.local>
2025-05-17 17:17:54 +0900

    Auto update CSV files

--- root.py
+++ root.py
@@ -40,6 +40,8 @@ def scrape_and_save():
                 status = "遅延"
             elif "運休" in info_text:
                 status = "運休"
+            elif "直通運転を中止" in info_text:
+                status = "直通運転中止"
             elif info_text == "平常運転":
                 # 平常運転なら結果に追加しない
                 print(f"ℹ️ {line_name} は平常運転のため結果に追加しません。")
